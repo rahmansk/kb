a98da7ba1c97b4a648ec25ec2d768cacf68b0cdb
Mohamed Rahman Shareff S <62998470+rahmansk@users.noreply.github.com>
2022-06-23 11:16:37 +0530

    Create kubeinit.sh

---
+++ kubeinit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubeadm init --ignore-preflight-errors=all
+
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.14/manifests/calico.yaml
+
+mkdir -p $HOME/.kube
+
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
